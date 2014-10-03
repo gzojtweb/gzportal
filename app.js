7120916851263818f8c6451feea2a4308eae207f
Elizar Pepino <jupenz@gmail.com>
2014-10-03 09:41:33 +0800

    update package.json and app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 'use strict';
-var express      = require('express');
-var path         = require('path');
-var logger       = require('morgan');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var mongoose     = require('mongoose');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var cors = require('express-cors');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -14,9 +16,15 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(cors({
+    allowedOrigins: ['http://localhost:8000', 'google.com']
+}));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,26 +38,26 @@ require('./models/Conferences');
 
 // include routes
 var routes = require('./routes/index');
-var news   = require('./routes/news');
+var news = require('./routes/news');
 var conferences  = require('./routes/conferences');
+var admin = require('./routes/admin');
 
 app.use('/api', routes);
 app.use('/api/news', news);
 app.use('/api/conferences', conferences);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -60,7 +68,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
